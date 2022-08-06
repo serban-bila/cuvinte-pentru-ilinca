e410ad887823d12a9ec5f1903cd041679a00493a
Serban <serban.bila@gmail.com>
2022-08-06 23:19:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let words = [
+  "Car",
+  "Bicycle",
+  "Banknote",
+  "Wallet",
+  "Blouse",
+  "Bag",
+  "Shirt",
+  "Helmet",
+  "Toothbrush",
+  "Key",
+  "Table",
+  "Coin",
+  "Trousers",
+  "Sweater",
+  "Shoe",
+  "Pen",
+  "Computer",
+  "Notebook",
+  "Desk",
+  "Pencil",
+  "Bookcase",
+  "Book",
+  "Chair",
+  "Backpack",
+  "Paper",
+  "Glue",
+  "Door",
+  "Ruler",
+  "Clock",
+  "Whiteboard",
+  "Window",
+  "Cupboard",
+  "Pillow",
+  "Coffee maker",
+  "Bed",
+  "Spoon",
+  "Blanket",
+  "Knife",
+  "Stove",
+  "Sink",
+  "Washing machine",
+  "Pot",
+  "Dish",
+  "Fridge",
+  "Sofa",
+  "Stool",
+  "Cup",
+  "Fork",
+  "Glass",
+];
+
+console.log(words.length);
+
+const randomWord = () => {
+  let x = Math.floor(Math.random() * 49) + 1;
+  console.log(x);
+  let word = words[x];
+  document.getElementById("result").innerHTML = word;
+  console.log(word);
+};
